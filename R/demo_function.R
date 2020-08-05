d95320c21d97bc16f1c86d4f4fb2139a05664f68
Vilmantas Gegzna <GegznaV@gmail.com>
2020-08-05 16:48:54 +0300

    Add demo unit test

--- R/demo_function.R
+++ R/demo_function.R
@@ -3,7 +3,17 @@
 #' You should delete this function.
 #'
 #' @return `NULL`
+#'
 #' @export
 demo_function <- function() {
+  NULL
+}
 
+# Unit tests -----------------------------------------------------------------
+
+test(demo_function) <- function() {
+  context("demo_function")
+
+  expect_silent(demo_function())
+  expect_equal(demo_function(), NULL)
 }
