a646ce6b8c8c37b42f73de63479b7fc3eff6f45f
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-07-15 23:31:27 +0300

    Create copy-github-labels-via-file.sh

---
+++ github-helpers/copy-github-labels-via-file.sh
@@ -0,0 +1,68 @@
+# This bash script:
+#   - reads labels from a GitHub repository and writes them to a file;
+#   - copies the labels from a file to a new repository.
+
+# Authors: Vilmantas Gegzna & Erick Oduniyi
+
+
+## Define variables
+
+# pat - GitHub PAT (personal access token)
+# You can generate PAT by running R code:
+#    usethis::create_github_token()
+# You can access PAT (if it is saved locally) by using R code:
+#    gitcreds::gitcreds_set()
+
+pat=???
+
+# Source repo:
+from_repo=r-hyperspec/hyperSpec
+# Destination repo:
+to_repo=r-hyperspec/hySpc.repo
+# Temporary directory name:
+dest_dir=github-helpers
+# File name for labels:
+new_json_file=r-hyperspec-github-labels.json
+
+
+## Make sure github-labels node package is installed
+npm install github-labels -g
+
+## Create directory
+mkdir ${dest_dir}
+
+## Construct file name and URL
+file=${dest_dir}/${new_json_file}
+url=https://api.github.com/repos/${from_repo}/labels
+
+## Get the new url if labels were moved to a new place
+## (this may happen with some repos):
+# url=$(
+#     curl -s $url |\
+#     sed '3!d'    |\ # get only row 3
+#     sed "s/^.*\"url\": \"\(.*\)\"\,.*$/\1/"
+#   )
+
+## Read labels and write to the file:
+curl -s $url |\
+  grep -ve '\"id\":' -      |\
+  grep -ve '\"node_id\":' - |\
+  grep -ve '\"url\":' -     |\
+  grep -ve '\"default\":' - > ${file}
+
+
+## To preview 6 rows (if needed):
+# head -6 ${file}
+
+
+### Select either A or B:
+
+## (A) Just copy new labels (w/o deleting the old ones):
+labels -c ${file} $to_repo -t $pat
+
+## (B) Delete all old labels and then copy new ones:
+# labels -c ${file} $to_repo -t $pat -f
+
+
+## Remove folder with all its contents (if needed):
+rm -rf ${dest_dir}
